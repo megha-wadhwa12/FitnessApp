5c664a37fc29082fa9b933bdf670251969f2734d
megha-wadhwa12 <megha.wadhwa@kalvium.community>
2024-03-01 12:31:30 +0530

    About Page Added

--- Frontend/FitHub/src/Components/About.jsx
+++ Frontend/FitHub/src/Components/About.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import { Box, Center, Text } from "@chakra-ui/react";
+import React from "react";
+import Theme from "./Theme";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <Box>
+        {/* Title */}
+        <Center
+          mt={20}
+          textAlign="center"
+          fontSize={"3xl"}
+          color={Theme.colors.primary[200]}
+          as={"b"}
+        >
+          Introducing FitHub !!
+        </Center>
+        {/* Description */}
+        <Text textAlign="center" mx={40} mb={50} mt={10}>
+        FitHub stands out as a groundbreaking prototype, dedicated to fostering a culture of wellness and enabling users to meticulously monitor their fitness endeavors, free from the constraints of conventional beauty ideals. Emphasizing holistic well-being, the platform inspires individuals to pursue long-term health goals, prioritizing overall wellness above fleeting physical appearances.
+        </Text>
+      </Box>
+    </>
+  );
+};
 
-export default About
+export default About;
--- Frontend/FitHub/src/Components/Home.jsx
+++ Frontend/FitHub/src/Components/Home.jsx
@@ -9,50 +9,54 @@ import {
   Text,
   useColorModeValue,
 } from "@chakra-ui/react";
-// import WorkingWomanSide from "./../assets/WorkingWomanSide.png";
 import WorkingManSide from "./../assets/WorkingManSide.png";
+import VectorImage from "./../assets/VectorImageHome.png";
 
 import Theme from "./Theme";
 import Navbar from "./Navbar";
+import About from "./About";
 
 const Home = () => {
   return (
     <Box>
       <Navbar />
-      <Flex gap={10} mt={10}>
-        <Box mx={40}>
+      <Flex gap={0} bg={Theme.colors.primary[100]} pt={5}>
+        <Box ml={"180px"}>
           <Text
-            alignSelf="flex-start"
+            // alignSelf="flex-start"
             mt={100}
-            color={Theme.colors.primary[100]}
+            // color={Theme.colors.primary[100]}
+            color={"white"}
             fontSize={"5xl"}
-            width={"380px"}
+            width={"420px"}
             textAlign={"left"}
+            fontFamily={'explore'}
           >
             The only hub for your total fitness !!
           </Text>
           <Button
-          color="white"
-          bg={Theme.colors.primary[100]}
-          borderWidth="2px"
-          borderColor={Theme.colors.primary[100]}
-          borderRadius="4px"
-          mt={10}
-          _hover={{
-            color: "white",
-            bg:Theme.colors.primary[100],
-            borderColor: Theme.colors.primary[100],
-            boxShadow: useColorModeValue(
-              "0px 0.25rem 0.75rem 0px rgb((203,155,133) / 26%)",
-              "0px 0.25rem 0.75rem 0px rgb((203,155,133) / 26%)"
-            ),
-          }}
-        >
-            Explore...
-        </Button>        </Box>
-        <Image src={WorkingManSide} width={430}/>
+            color={Theme.colors.primary[100]}
+            bg="white"
+            borderWidth="2px"
+            borderColor={Theme.colors.primary[100]}
+            borderRadius="4px"
+            mt={10}
+            _hover={{
+              color: "white",
+              bg: Theme.colors.primary[100],
+              borderColor: Theme.colors.primary[100],
+              boxShadow: useColorModeValue(
+                "0px 0.25rem 0.75rem 0px rgb((203,155,133) / 26%)",
+                "0px 0.25rem 0.75rem 0px rgb((203,155,133) / 26%)"
+              ),
+            }}
+          >
+            Explore Now
+          </Button>
+        </Box>
+        <Image src={VectorImage} width={600} height={500}/>
       </Flex>
-
+      <About />
     </Box>
   );
 };
